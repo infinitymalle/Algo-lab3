3425314f581cad5b508407d6e20d61aee6ae1ca3
Malkolm <amouli-0@student.ltu.se>
2021-12-06 17:18:09 +0100

    rip

--- Labb3.py
+++ Labb3.py
@@ -1,1 +1,25 @@
 #peepee poopoo
+class V:
+    def __init__(self, value, parent, id, size, left = None, right = None):
+        self.left  = left
+        self.right = right
+        self.value = value
+        self.parent = parent
+        self.id = id
+        self.size = size
+
+class D:   
+    def __init__(self, list):
+        self.list = list
+
+    def insert(x):
+        while(x != None):
+            self.x = beep boop
+
+    def delete():
+
+    def search():
+
+    def transplant():
+        
+        
