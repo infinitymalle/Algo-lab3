4683a9d549de69850741c5a887320e65783a438f
Tall Fellow <34690005+Tall-Fellow@users.noreply.github.com>
2021-12-07 12:41:33 +0100

    Added base code for BST, insertion and tree display

--- Labb3.py
+++ Labb3.py
@@ -1,25 +1,50 @@
-#peepee poopoo
-class V:
-    def __init__(self, value, parent, id, size, left = None, right = None):
-        self.left  = left
-        self.right = right
-        self.value = value
-        self.parent = parent
-        self.id = id
-        self.size = size
+class BST:
+    def __init__(self, keys):
+        self.root = self.Node(keys[0], None)
+        for i in range(1, len(keys)):
+            self.insert(keys[i], self.root)
 
-class D:   
-    def __init__(self, list):
-        self.list = list
+    def insert(self, newKey, currNode):
+        if currNode.key > newKey:
+            if currNode.left is None:
+                currNode.left = self.Node(newKey, currNode)
+            else:
+                self.insert(newKey, currNode.left)
+        elif currNode.key < newKey:
+            if currNode.right is None:
+                currNode.right = self.Node(newKey, currNode)
+            else:
+                self.insert(newKey, currNode.right)
 
-    def insert(x):
-        while(x != None):
-            self.x = beep boop
+    def balanceBST(self):
+        return -1
+        #code
 
-    def delete():
+    # NOT TESTED!
+    def printTree(self, currNode = -1):
+        if currNode is None:
+            return 0
 
-    def search():
-
-    def transplant():
+        if currNode == -1:
+            currNode = self.root
+            print("\nThe current tree:\n")
         
-        
+        # Check for None!!!!!!!
+        print(currNode.key, "\n", currNode.left.key, currNode.right.key)
+        self.printTree(currNode.left)
+        self.printTree(currNode.right)
+
+    class Node:
+        def __init__(self, key, parent, left = None, right = None):
+            self.key    = key
+            self.parent = parent
+            self.left   = left
+            self.right  = right
+            #self.size   = self.calcSize()
+
+        def calcSize(self):
+            return 0
+
+keysToSort = [10, 5, 15, 0, 20]
+tree = BST(keysToSort)
+tree.printTree()
