e21ec6f56a77f153cc22a19c445b80e1bf24f1ca
Tall Fellow <34690005+Tall-Fellow@users.noreply.github.com>
2021-12-07 20:11:27 +0100

    Fixed func for printing Tree, started on node size calc

--- Labb3.py
+++ Labb3.py
@@ -3,6 +3,7 @@ class BST:
         self.root = self.Node(keys[0], None)
         for i in range(1, len(keys)):
             self.insert(keys[i], self.root)
+        # Calc size for every node        
 
     def insert(self, newKey, currNode):
         if currNode.key > newKey:
@@ -20,17 +21,21 @@ class BST:
         return -1
         #code
 
-    # NOT TESTED!
     def printTree(self, currNode = -1):
         if currNode is None:
             return 0
 
         if currNode == -1:
             currNode = self.root
-            print("\nThe current tree:\n")
+            print("\nThe current tree:")
         
-        # Check for None!!!!!!!
-        print(currNode.key, "\n", currNode.left.key, currNode.right.key)
+        print("")
+        print("    ", currNode.key)
+        print("  /\t\\")
+        print(
+            currNode.left.key if currNode.left is not None else "None",
+            "\t", currNode.right.key if currNode.right is not None else "None"
+        )
         self.printTree(currNode.left)
         self.printTree(currNode.right)
 
@@ -40,10 +45,19 @@ class BST:
             self.parent = parent
             self.left   = left
             self.right  = right
-            #self.size   = self.calcSize()
+            self.size   = self.calcSize(self)
 
-        def calcSize(self):
-            return 0
+        # NEEDS FURTHER TESTING
+        def calcSize(self, node):
+            counter = 0
+        
+            if node is None:
+                return 0
+
+            counter += self.calcSize(node.left)
+            counter += self.calcSize(node.right)
+
+            return counter + 1
 
 keysToSort = [10, 5, 15, 0, 20]
 tree = BST(keysToSort)
