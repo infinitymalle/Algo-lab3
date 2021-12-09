ce0d5c19dbb14b2e9c351e02e27536e21a24ef4a
Tall Fellow <34690005+Tall-Fellow@users.noreply.github.com>
2021-12-09 11:32:31 +0100

    C-C-Changes

--- Labb3.py
+++ Labb3.py
@@ -14,7 +14,6 @@ def CreateData():
     #print("test", dataset)
     return dataset
 
-
 class BST:
     def __init__(self, keys, c):
         self.c    = c
@@ -23,61 +22,61 @@ class BST:
             self.insert(keys[i], self.root)
 
         # Calc size for every node
-        self.root.calcSize(self.root)
+        self.calcSize(self.root)
 
+    def calcSize(self, node):
+        if node is None:
+            return 0
+        
+        counter = 0
+        counter += self.calcSize(node.left)
+        counter += self.calcSize(node.right)
+        node.size = counter + 1
+        return node.size
+        
     def insert(self, newKey, currNode):
-        newLeafNode = self._createLeaf(newKey, currNode)
-        #if newLeafNode is None:
-        #    return
-
-    def _balanceBST(self, node):
-        parentSize = node.parent.left.size + node.parent.right.size
-        if (node.parent.left.size * self.c > parentSize or
-            node.parent.right.size * self.c > parentSize):
-            #Code
-
-            return
-
-        self._balanceBST(node.parent)
-
-    def _createLeaf(self, newKey, currNode):            
-
         cSize = currNode.size * self.c
         if currNode.key > newKey and currNode.left.size < cSize:
             if currNode.left is None:
                 currNode.left = self.Node(newKey, currNode)
                 return currNode.left
             else:
-                self._createLeaf(newKey, currNode.left)
+                self.insert(newKey, currNode.left)
 
         else:
-            newNode = self.Node(newKey, currNode.parent) # Create new node, set parent
-            if currNode.parent.left.key == currNode.key: # Set currNode.parent left or right pointer to newNode
-                currNode.parent.left = newNode
-            else:
-                currNode.parent.right = newNode
-            currNode.parent = newNode # Set currNode.parent to newNode
-            newNode.left = currNode.left
-            newNode.left.parent = newNode
-            newNode.right = currNode
+            self._rotateLeft(newKey, currNode)
 
         if currNode.key < newKey and currNode.right.size < cSize:
             if currNode.right is None:
                 currNode.right = self.Node(newKey, currNode)
                 return currNode.right
             else:
-                self._createLeaf(newKey, currNode.right)
+                self.insert(newKey, currNode.right)
 
         else:
-            newNode = self.Node(newKey, currNode.parent) # Create new node, set parent
-            if currNode.parent.left.key == currNode.key: # Set currNode.parent left or right pointer to newNode
-                currNode.parent.left = newNode
-            else:
-                currNode.parent.right = newNode
-            currNode.parent = newNode # Set currNode.parent to newNode
-            newNode.right = currNode.right
-            newNode.right.parent = newNode
-            newNode.left = currNode
+            self._rotateRight(newKey, currNode)
+            
+    def _rotateLeft(self, newKey, currNode):
+        newNode = self.Node(newKey, currNode.parent)
+        if currNode.parent.left.key == currNode.key:
+            currNode.parent.left = newNode
+        else:
+            currNode.parent.right = newNode
+        currNode.parent = newNode
+        newNode.left = currNode.left
+        newNode.left.parent = newNode
+        newNode.right = currNode
+
+    def _rotateRight(self, newKey, currNode):
+        newNode = self.Node(newKey, currNode.parent)
+        if currNode.parent.left.key == currNode.key:
+            currNode.parent.left = newNode
+        else:
+            currNode.parent.right = newNode
+        currNode.parent = newNode
+        newNode.right = currNode.right
+        newNode.right.parent = newNode
+        newNode.left = currNode
 
     def printTree(self, currNode = -1):
         if currNode is None:
@@ -104,16 +103,6 @@ class BST:
             self.left   = None
             self.right  = None
             self.size   = 1
-
-        def calcSize(self, node):
-            if node is None:
-                return 0
-            
-            counter = 0
-            counter += self.calcSize(node.left)
-            counter += self.calcSize(node.right)
-            node.size = counter + 1
-            return node.size
 
 keysToSort = CreateData()
 tree = BST(keysToSort, 0.5)
